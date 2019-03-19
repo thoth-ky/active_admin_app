f28312e5afd266a2d0d9ceb7df731cf8a7e1e968
Mutuku Kyalo <28805113+jmutuku95@users.noreply.github.com>
2019-03-19 13:56:00 +0300

    Setup devise and articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < InheritedResources::Base
+
+  def create
+    @article = Article.new(article_params)
+    @article.author = current_user
+    create!
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+  devise_for :users, ActiveAdmin::Devise.config
+  root to: "articles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
