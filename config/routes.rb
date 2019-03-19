917382d662b3dcdf8facdf4a256ce3a2e097bb7f
Mutuku Kyalo <28805113+jmutuku95@users.noreply.github.com>
2019-03-19 14:40:36 +0300

    Fix devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :articles
-  devise_for :users, ActiveAdmin::Devise.config
   root to: "articles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
